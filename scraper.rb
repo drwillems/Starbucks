003040de3efbc70d44a33b6651169f1b257ee29f
drwillems <drwillems@users.noreply.github.com>
2016-10-21 14:18:21 +1300

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,16 +1,17 @@
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
+require 'scraperwiki'
+require 'mechanize'
 #
-# agent = Mechanize.new
+agent = Mechanize.new
 #
 # # Read in a page
-# page = agent.get("http://foo.com")
+page = agent.get("https://www.starbucks.com/store-locator/store/1")
 #
 # # Find somehing on the page using css selectors
-# p page.at('div.content')
+p page.at('.mb1')
+
 #
 # # Write out to the sqlite database using scraperwiki library
 # ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
